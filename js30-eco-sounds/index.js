bb4896becee7e444041349745063998215198553
Natalia Kondratieva <kondratieva07@gmail.com>
2022-02-07 19:02:20 +0300

    feat: add change mp3 task

--- js30-eco-sounds/index.js
+++ js30-eco-sounds/index.js
@@ -28,75 +28,80 @@ function toggleClass() {
 play.addEventListener('click', toggleClass);
 
 
-
-
-
 //change pictures
 
-/*
+
 const navItem = [...document.querySelectorAll('.nav_item')];
 const navList = document.querySelector('.nav_list'); //Родитель
-const images = document.querySelectorAll('.main');
+const images = document.querySelector('.main');
 
 function changeImage(event, index) {
     if (event.target.hasAttribute('data_item')) {
         
-      images.style.backgroundImage = `url(../assets/img/${event.target.dataset.item}/${index}.jpg)` 
-   }
+      images.style.backgroundImage = `url('../assets/img/solovey.jpg')` ;
+      
+   } 
 }
-
-navList.addEventListener('click', changeImage)*/
-
-
-console.log(`Общий балл:40\nВёрстка +10\nАктивный в данный момент интерактивный элемент выделяется стилем +10\nКнопка Play/Pause +20`)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const suscribe = () => {
-    navList.addEventListener('click', changeImage)
-
-}
- suscribe();
-
-/*
-function changeImage(event) {
-    if (event.target.classList.contains('nav_item')) {
-    audio.src = `./assets/audio/${event}/${index + 1}.mp3`;
-    images.style.backgroundImage = `url(/assets/img/${event}/${index + 1}.jpg)`;
-   }
-}
-
 
 navList.addEventListener('click', changeImage)
-*/
-/*
-function changeBgImg(){
-    block.style.backgroundImage = "url('https://cs7062.vk.me/c540107/v540107359/2729/fYQlS_23QdA.jpg')";
-}*/
-/*
-navList.addEventListener('click', (event) => {
-    changeImage(event);
-});
-
-function changeImage(event) {
-if(event.target.classList.contains('nav_item')) {
-  // здесь код функции, меняющей src изображений
-  images.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.item}/${index + 1}.jpg`);
-} 
-}*/
 
 
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const icon = document.querySelector('.icon');
+
+    
+one.onclick = function (images) {
+    console.log('work');
+    audio.src = './assets/audio/solovey.mp3';
+    audio.play();
+    play.classList.toggle('pause');
+    
+    
+    //images.style.backgroundImage = 'red';
+    //images.style.paddingBottom = '500px';    
+}
+
+two.onclick = function (images) {
+    console.log('play');
+    audio.src = './assets/audio/drozd.mp3';
+    audio.play();
+    
+}
+
+three.onclick = function (images) {
+    console.log('play');
+    audio.src = './assets/audio/malinovka.mp3';
+    audio.play();
+    
+}
+four.onclick = function (images) {
+    console.log('play');
+    audio.src = './assets/audio/zhavoronok.mp3';
+    audio.play();
+    
+}
+five.onclick = function (images) {
+    console.log('play');
+    audio.src = './assets/audio/slavka.mp3';
+    audio.play();
+    
+}
+
+icon.onclick = function (images) {
+    console.log('play');
+    audio.src = './assets/audio/forest.mp3';
+    audio.play();
+    
+}
+/*function newBackGround (element,background) {
+    element.style.backgroundImage = "url("+background+")";
+ }
+ 
+ newBackground (myElement,"newBackground.jpg");*/
 
 
+ console.log(`Общий балл:52\nВёрстка +10\nАктивный в данный момент интерактивный элемент выделяется стилем +10\nПри кликах по интерактивным элементам меняется изображение + 2 балл(а)\nКнопка Play/Pause +20`)
